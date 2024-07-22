ed38a9a025ab3af1c9894a294724202d96786748
templaterscaff <templater@dinocloudconsulting.com>
2024-07-22 12:02:40 -0300

    First commit

--- .github/workflows/README.md
+++
@@ -1,8 +0,0 @@
-# Github Actions
-
-Do not forget to add the following github repository secrets so that the terraform plan works:
-
-* SANDBOX1_ROLE_ARN
-* TERRAFORM_INTERNAL_MODULES_TOKEN
-
-For more information take a look at the [documentation](https://app.clickup.com/3016250/v/dc/2w1hu-45397/2w1hu-27837).
--- main.tf
+++ main.tf
@@ -1,20 +1,22 @@
-locals {
-  common_name = lower("${var.customer}-${var.team}-${var.component}-infra-${var.environment}")
-  #Tags
-  common_tags = {
-    Project     = var.component
-    Customer    = var.customer
-    Team        = var.team
-    CostCenter  = var.team
-    Environment = var.environment
-    Provisioner = "terraform"
-    Common_name = local.common_name
-  }
+################# Control Tower Guardrails. Target: AFT OU ###################
+resource "aws_controltower_control" "cloud_formation_required_guardrail" {
+  control_identifier = "arn:aws:controltower:us-east-1::control/WTDSMKDKDNLE"
+  target_identifier  = "arn:aws:organizations::211125622358:ou/o-sgbmeqwqhx/ou-qyg8-lcenb5y4"
 }
 
-module "" {
-  source  = ""
-  version = ""
-  name    = local.common_name
-  tags    = local.common_tags
+resource "aws_controltower_control" "s3_kms_encryption_required" {
+  control_identifier = "arn:aws:controltower:us-east-1::control/AZDYETJYRMPN"
+  target_identifier  = "arn:aws:organizations::211125622358:ou/o-sgbmeqwqhx/ou-qyg8-lcenb5y4"
+  depends_on         = [aws_controltower_control.cloud_formation_required_guardrail]
 }
+
+resource "aws_controltower_control" "s3_bucket_public_read_prohibited_guardrail" {
+  control_identifier = "arn:aws:controltower:us-east-1::control/AWS-GR_S3_BUCKET_PUBLIC_READ_PROHIBITED"
+  target_identifier  = "arn:aws:organizations::211125622358:ou/o-sgbmeqwqhx/ou-qyg8-lcenb5y4"
+}
+
+resource "aws_controltower_control" "s3_bucket_disallow_delete_without_mfa" {
+  control_identifier = "arn:aws:controltower:us-east-1::control/AWS-GR_RESTRICT_S3_DELETE_WITHOUT_MFA"
+  target_identifier  = "arn:aws:organizations::211125622358:ou/o-sgbmeqwqhx/ou-qyg8-lcenb5y4"
+}
+################# /Control Tower Guardrails. Target: AFT OU ###################
