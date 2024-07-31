1cc07d64d7022694ac3c66d20c9fc6b787d191b3
templaterscaff <templater@dinocloudconsulting.com>
2024-07-31 14:57:37 -0300

    fix guardrails module

--- modules/control_tower_guardrails/variables.tf
+++ modules/control_tower_guardrails/variables.tf
@@ -6,7 +6,7 @@ variable "mpa_account_id" {
   description = "MPA account ID"
   type        = string
 }
-variable "aft_ou_id" {
-  description = "AFT OU ID"
+variable "production_ou_id" {
+  description = "Production organization unit ID"
   type        = string
 }
